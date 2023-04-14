a236941eb74d846a9267cfcab945eda0398a456d
FuFuFsy <fsyaxx@gmail.com>
2023-04-14 16:28:33 +0100

    Initial commit

---
+++ authlogin.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# 做登录限制，有些页面只有登录之后才能访问
+# 通过定义装饰器实现
+from functools import wraps
+from flask import session, redirect, url_for
+
+
+def login_required(func):
+    @wraps(func)  # 防止传入的函数的一些签名丢失
+    def wrapper(*args, **kwargs):
+        if session.get("email"):
+            # 当前处于登陆状态
+            print(session.get('email'))
+            return func(*args, **kwargs)
+        else:
+            return redirect(url_for("login"))
+
+    return wrapper
+
